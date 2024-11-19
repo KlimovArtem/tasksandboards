806fd4fa3e5b92e71596c38f0cb7e4e1546d1854
Artem Klimov <grinchomsk@gmail.com>
2024-11-19 13:17:06 +0600

    1) Add template for board form; 2) Set up the `get_queryset_method` of the `BoardList` view to get all board objects that are a subclass of the abstract `Board` class.

--- src/tasksandboards/boards/views.py
+++ src/tasksandboards/boards/views.py
@@ -1,17 +1,20 @@
-from calendar import c
 from django.db.models import QuerySet
+from django.http import Http404
+from django.utils.translation import gettext_lazy
 from django.views.generic import TemplateView, ListView
 
 from boards.models import *
 
 
 class BoardsList(ListView):
+    context_object_name = 'boards'
     template_name = 'boards/boards_list.html'
 
     def get_queryset(self):
         boards = QuerySet()
-        [boards.union(board.objects.all()) for board in Board.__subclasses__()]
-        return boards
+        for board in Board.__subclasses__():
+            boards.union(board.objects.all())
+        return ['Канбан', 'Блокнот']
 
 
 class StartPageView(TemplateView):
